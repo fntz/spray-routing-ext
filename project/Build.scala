583cf110f833c0fef7a415e83650d50eed31a461
mike <fantazuor@gmail.com>
2014-04-02 21:13:15 +0300

    update to spray 1.3.1

--- project/Build.scala
+++ project/Build.scala
@@ -12,16 +12,29 @@ object SprayRoutingExtBuild extends Build {
       scalaVersion := "2.10.3",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies ++= Seq(
-        "com.typesafe.akka" %% "akka-actor" % "2.2.0",
+        "com.typesafe.akka" %% "akka-actor" % "2.2.4",
         "org.scala-lang"    %  "scala-reflect"   % "2.10.3",
         "org.scalamacros"   %  "quasiquotes_2.10.3" % "2.0.0-M3",
-        "io.spray"          %  "spray-can"     % "1.2.0",
-        "io.spray"          %  "spray-routing" % "1.2.0",
+        "io.spray"          %  "spray-can"     % "1.3.1",
+        "io.spray"          %  "spray-routing" % "1.3.1",
         "org.scalatest"     %% "scalatest" % "2.0" % "test",
-        "io.spray"          %  "spray-testkit" % "1.2.0" % "test"
+        "io.spray"          %  "spray-testkit" % "1.3.1" % "test"
       ),
       addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0-M3" cross CrossVersion.full)
     )
   )
 
+  lazy val sampleProject = Project(
+    id = "sample",
+    base = file("sample"),
+    settings = Project.defaultSettings ++ Seq(
+      name := "ext-sample",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.10.3",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      libraryDependencies ++= Seq(
+
+      )
+    )
+  ) dependsOn(mainProject)
 }
